64fb1dbb014336039949a29c2ee6b0f4f27923b6
malkoG <rijgndqw012@gmail.com>
2024-08-04 14:30:59 +0900

    Add pinia for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  compatibilityDate: "2024-07-03"
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@pinia/nuxt',
+  ],
+  compatibilityDate: "2024-07-03",
 })
