8ac327eeab5b89ccca10c143afee887da9eee751
malkoG <rijgndqw012@gmail.com>
2024-08-04 17:29:58 +0900

    Refactor gallery to use status-post component for displaying statuses

---
+++ components/status-post.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Carousel, Slide } from 'vue3-carousel'
+
+import type { Status } from '~/models/status';
+
+const props = defineProps({
+  status: {
+    type: Object as PropType<Status>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="card bg-base-100 w-96 shadow-xl">
+    <carousel :items-to-show="1">
+      <slide v-for="attachment in status.mediaAttachments" :key="attachment.url">
+        <img class="w-full" :src="attachment.url || ''" alt="Shoes" />
+      </slide>
+    </carousel>
+  </div>
+  <div class="card-body">
+    <p>{{ status.content }}</p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.carousel {
+
+  &__next,
+  &__prev {
+    display: none;
+  }
+}
+</style>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-import { useStatus } from "~/hooks/statuses";
 
-const result = await useStatus("713777587527607174");
+import { useStatus } from "~/hooks/statuses";
+import StatusPost from "~/components/status-post.vue";
+import type { Status } from "~/models/status";
+
+const result = await useStatus("713777587527607174") as Status;
 </script>
 <template>
-  <div v-for="attachment in result.mediaAttachments">
-    <div class="card bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img :src="attachment.url || ''" alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <p>{{ result.content }}</p>
-        <div class="card-actions justify-end">
-        </div>
-      </div>
-    </div>
-  </div>
+  <status-post :status="result" />
 </template>
