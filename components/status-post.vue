b9700fa124a93f710bf7bc643ae49106fd6c9638
malkoG <rijgndqw012@gmail.com>
2024-08-10 19:51:34 +0900

    Enhanced status-post layout with AspectRatio; refined carousel styles
    - Added AspectRatio component for consistent media display
    - Updated carousel styles for improved layout and responsiveness

--- components/status-post.vue
+++ components/status-post.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Carousel, Slide } from 'vue3-carousel'
+import { AspectRatio } from 'radix-vue';
 
 import type { Status } from '~/models/status';
 
@@ -12,22 +13,51 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="card bg-base-100 w-96 shadow-xl">
-    <carousel :items-to-show="1">
-      <slide v-for="attachment in status.mediaAttachments" :key="attachment.url">
-        <img class="w-full" :src="attachment.url || ''" alt="Shoes" />
-      </slide>
-    </carousel>
-  </div>
-  <div class="card-body">
-    <div v-html="status.content"></div>
-    <div class="card-actions justify-end">
+  <div class="w-full py-12">
+    <div class="bg-gray-700">
+      <AspectRatio :ratio="1">
+        <div class="w-full h-full flex justify-center items-center">
+          <div class="bg-base-100 w-full h-full shadow-xl">
+            <carousel :items-to-show="1">
+              <slide v-for="attachment in status.mediaAttachments" :key="attachment.url">
+                <img class="w-full" :src="attachment.url || ''" />
+              </slide>
+            </carousel>
+          </div>
+        </div>
+      </AspectRatio>
+    </div>
+    <div class="card-body">
+      <div v-html="status.content"></div>
+      <div class="card-actions justify-end">
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .carousel {
+  width: 100%;
+  height: 100%;
+
+  &__viewport {
+    width: 100%;
+    height: 100%;
+    vertical-align: middle;
+  }
+
+  &__track {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__slide {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
   &__next,
   &__prev {
