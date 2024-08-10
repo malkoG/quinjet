803f3e91c6d02ac557e11adca3fd57f5739c4480
malkoG <rijgndqw012@gmail.com>
2024-08-10 20:26:35 +0900

    Refactor grid layout for home page, improving alignment of StatusPost component

--- pages/home.vue
+++ pages/home.vue
@@ -13,14 +13,14 @@ const statuses = await masto.v1.timelines.home.list() as Status[];
 </script>
 <template>
   <QuinjetPage>
-    <div class="container mx-auto grid grid-cols-3 lg:grid-cols-4 gap-x-4">
-      <div id="profile" class="col-span-1"></div>
+    <div class="container mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
+      <div id="profile" class="md:col-span-1"></div>
       <div id="feed" class="col-span-2">
-        <div v-for="status in statuses">
+        <div class="flex justify-center" v-for="status in statuses">
           <status-post :status="status" />
         </div>
       </div>
-      <div id="notifications" class="col-span-1"></div>
+      <div id="notifications" class="hidden lg:block lg:col-span-1"></div>
     </div>
   </QuinjetPage>
 </template>
