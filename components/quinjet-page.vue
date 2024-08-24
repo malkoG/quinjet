4f14cad12add9c31e2e880765bd1ebfe746cd5eb
malkoG <rijgndqw012@gmail.com>
2024-08-24 14:30:36 +0900

    Enhance quinjet-page container: add full screen height and vertical scrolling

--- components/quinjet-page.vue
+++ components/quinjet-page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white">
+  <div class="bg-white h-screen overflow-y-scroll">
     <header class="text-center text-lg font-black py-12">
       Placeholder for navigation
     </header>
