e7bc899fd2c12a69d38c62bac6f9aae70572b988
malkoG <rijgndqw012@gmail.com>
2024-08-27 23:06:10 +0900

    Refactor layout by extracting shared container component

---
+++ components/quinjet-container.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
+    <div id="profile" class="md:col-span-1"></div>
+    <div id="feed" class="col-span-2">
+      <slot/>
+    </div>
+    <div id="notifications" class="hidden lg:block lg:col-span-1"></div>
+  </div>
+</template>
