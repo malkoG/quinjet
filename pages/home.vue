d5427b1f667db6f11442dfcca93fe8defa87861d
malkoG <rijgndqw012@gmail.com>
2024-08-27 23:27:45 +0900

    Introduce tanstack query

--- pages/home.vue
+++ pages/home.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
+import { createRestAPIClient } from "masto";
+import { useQuery } from "@tanstack/vue-query";
 
 import StatusPost from "~/components/status-post.vue";
 import QuinjetContainer from "~/components/quinjet-container.vue";
 
 import type { Status } from "~/models/status";
-import { createRestAPIClient } from "masto";
 
 const masto = createRestAPIClient({
   url: "https://pixelfed.social",
   accessToken: sessionStorage.getItem("accessToken") as string,
 });
 
-const statuses = await masto.v1.timelines.home.list() as Status[];
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ['home'],
+  queryFn: async () => {
+    const statuses = await masto.v1.timelines.home.list() as Status[];
+    return statuses;
+  },
+})
 </script>
 <template>
   <QuinjetPage>
     <QuinjetContainer>
-      <div class="flex justify-center" v-for="status in statuses">
+      <div class="flex justify-center" v-for="status in data">
         <status-post :status="status" />
       </div>
     </QuinjetContainer>
