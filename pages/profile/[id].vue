58174833a6002c5db772d74cdb132975eaa9fa34
malkoG <rijgndqw012@gmail.com>
2024-08-31 17:07:06 +0900

    Add display name and note to mobile profile view

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -35,7 +35,7 @@ const statuses = await fetchStatuses()
     <div class="lg:container mx-auto block sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
       <div id="profile" class="col-span-2 md:col-span-1 mx-4">
         <div class="flex sm:block">
-          <div id="profile-image" class="w-48 sm:w-64 md:w-full pr-12">
+          <div id="profile-image" class="w-48 sm:w-64 md:w-full pr-4 sm:pr-12 flex-2">
             <AspectRatio :ratio="1">
               <img class="max-w-48 max-h-48 sm:max-w-64 sm:max-h-64 w-full h-full rounded-full" :src="profile.avatar"
                 alt="Profile Image" />
@@ -47,10 +47,14 @@ const statuses = await fetchStatuses()
             <div>
             </div>
           </div>
-          <div class="mt-4 w-full">
+          <div class="mt-4 w-full flex-3">
             <ProfileIndicator :account="profile" />
           </div>
         </div>
+      </div>
+      <div class="mx-4 block sm:hidden py-4">
+        <span class="text-xl font-black">{{ profile.displayName }}</span>
+        <div v-html="profile.note"></div>
       </div>
       <div id="feed" class="col-span-2 lg:col-span-3 mx-4">
         <div class="grid grid-cols-3 gap-4">
