168780d02daaf4cbd515a28390c4b2e85d5af4a7
malkoG <rijgndqw012@gmail.com>
2024-08-31 16:48:56 +0900

    Add profile-indicator component to display posts, followers, and following counts

---
+++ components/profile-indicator.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+import type { Account } from '~/models/account';
+
+const props = defineProps({
+  account: {
+    type: Object as PropType<Account>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <!-- stretched inside flex --->
+  <div class="grid grid-cols-3">
+    <div class="col-span-1 text-center">
+      <div class="text-3xl font-black">{{ account.statusesCount }}</div>
+      <div class="text-md text-gray-500">Posts</div>
+    </div>
+    <div class="col-span-1 text-center">
+      <div class="text-3xl font-bold">{{ account.followersCount }}</div>
+      <div class="text-md text-gray-500">Followers</div>
+    </div>
+    <div class="col-span-1 text-center">
+      <div class="text-3xl font-bold">{{ account.followingCount }}</div>
+      <div class="text-md text-gray-500">Following</div>
+    </div>
+  </div>
+</template>
