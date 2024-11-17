661db89a86b10db6b9c0c3976a65b7f8b99fc211
malkoG <rijgndqw012@gmail.com>
2024-11-17 16:22:26 +0900

    Support ripple effect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import Aura from '@primevue/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
@@ -5,21 +6,39 @@ export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss",
     '@pinia/nuxt',
+    '@primevue/nuxt-module'
   ],
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }
+  },
   compatibilityDate: "2024-07-03",
   app: {
     head: {
       script: [
         {
           src: "https://unpkg.com/@material-tailwind/html@latest/scripts/dialog.js",
-          async: true
+          async: true,
+
+          tagPosition: "bodyClose",
         },
         {
           src: "https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js",
-          async: true
+          async: true,
+          tagPosition: "bodyClose",
         }
 
       ]
     }
-  }
+  },
 })
--- pages/compose.vue
+++ pages/compose.vue
@@ -166,7 +166,7 @@ const onDragLeave = () => {
           class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2">
           Open Dialog XS
         </button>
-        <button type="button" data-ripple-light="true"
+        <button v-ripple type="button" data-ripple-light="true"
           class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
           Button
         </button>
