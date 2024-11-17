3555ce9bd58d290bea258828890dc07eb4274df7
malkoG <rijgndqw012@gmail.com>
2024-11-17 15:59:35 +0900

    Test material-tailwind feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,19 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
   ],
   compatibilityDate: "2024-07-03",
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://unpkg.com/@material-tailwind/html@latest/scripts/dialog.js",
+          async: true
+        },
+        {
+          src: "https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js",
+          async: true
+        }
+
+      ]
+    }
+  }
 })
